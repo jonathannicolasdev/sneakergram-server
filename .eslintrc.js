d12b78e8a8019966c52a33f2d37d94a6be8a2860
M Haidar Hanif <git@mhaidarhanif.com>
2020-07-30 12:34:22 +0700

    Configure prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  plugins: ['prettier'],
+  plugins: ["prettier"],
+  extends: "eslint:recommended",
   env: {
-    es6: true,
+    commonjs: true,
+    es2020: true,
     node: true,
   },
   parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2017,
+    ecmaVersion: 11,
   },
   rules: {
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
   },
-}
+};
