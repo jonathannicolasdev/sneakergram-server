bd01b63a28a42d574863a6afc96b9d345839319d
M Haidar Hanif <git@mhaidarhanif.com>
2020-07-30 13:11:52 +0700

    Create sneaker model

---
+++ middlewares/sneakers/model.js
@@ -0,0 +1,40 @@
+const mongoose = require("../../config/mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
+const Schema = mongoose.Schema;
+
+const SneakerSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Sneaker name is required"],
+      min: [2, "Sneaker name is too short"],
+      max: [100, "Sneaker name is too long"],
+    },
+    imageUrl: {
+      type: String,
+      required: [true, "Sneaker image URL is required"],
+    },
+    style: { type: String },
+    colorway: { type: String },
+    retailPrice: { type: String },
+    releaseDate: { type: String },
+    size: { type: String },
+    location: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// plug the AutoIncrement plugin into the schema to create auto incremented id
+// id is different with _id
+// inc_field is to track which id to increment
+SneakerSchema.plugin(AutoIncrement, {
+  id: "sneakers_counter",
+  inc_field: "id",
+});
+
+// Sneaker model => sneakers collection
+const Sneaker = mongoose.model("Sneaker", SneakerSchema);
+
+module.exports = Sneaker;
