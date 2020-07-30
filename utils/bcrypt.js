4b39e22d94e132c4f2f37b4fc3e39a397052b44a
M Haidar Hanif <git@mhaidarhanif.com>
2020-07-30 12:34:36 +0700

    Lint utils

--- utils/bcrypt.js
+++ utils/bcrypt.js
@@ -1,14 +1,14 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 
 module.exports = {
   //////////////////////////////////////////////////////////////////////////////
   // ENCRYPT PASSWORD, RETURN SALT & ENCRYPTED PASSWORD
   encryptPassword: async (plainPassword) => {
     // generate salt
-    const salt = await bcrypt.genSalt(10)
+    const salt = await bcrypt.genSalt(10);
     // encrypt/hash the plain password with generated salt
     // with becrypt, we have to use the salt
-    const encryptedPassword = await bcrypt.hash(plainPassword, salt)
+    const encryptedPassword = await bcrypt.hash(plainPassword, salt);
 
     // return both salt & encryptedPassword as an object
     // will be used in the register middleware
@@ -17,16 +17,16 @@ module.exports = {
     return {
       salt,
       encryptedPassword,
-    }
+    };
   },
 
   //////////////////////////////////////////////////////////////////////////////
   // COMPARE ENCRYPTED PASSWORD WITH PLAIN PASSWORD
   comparePassword: async (password, encryptedPassword) => {
     // slow process to determine password is matched
-    const isAuthenticated = await bcrypt.compare(password, encryptedPassword)
+    const isAuthenticated = await bcrypt.compare(password, encryptedPassword);
 
     // result is either true or false
-    return isAuthenticated
+    return isAuthenticated;
   },
-}
+};
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,4 +1,8 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+
+function hello() {
+  console.log(hello1);
+}
 
 module.exports = {
   //////////////////////////////////////////////////////////////////////////////
@@ -10,16 +14,16 @@ module.exports = {
       name: foundUser.name, // name: user's full name
       email: foundUser.email, // email: user's email
       // iat: issued at: will be created automatically as UNIX timestamp
-    }
+    };
 
     // create the token using jwt.sign()
     // will be expired in 1 day
     const token = await jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: '1d',
-    })
+      expiresIn: "1d",
+    });
 
     // return token to be used later by the frontend
-    return token
+    return token;
   },
 
   //////////////////////////////////////////////////////////////////////////////
@@ -28,16 +32,16 @@ module.exports = {
     // use try catch to prevent app crashing
     try {
       // verify token with the same secret from backend
-      const decodedToken = await jwt.verify(token, process.env.JWT_SECRET)
+      const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
       // decoded token example:
       // { sub: '5c6fd1eb739522a11e19923e', iat: 1550834260 }
 
       // return decoded token object is fine
-      return decodedToken
+      return decodedToken;
     } catch (error) {
       // catch the error if it's happen
       // such as when the token is invalid or decodedToken is false
-      return error
+      return error;
     }
   },
-}
+};
