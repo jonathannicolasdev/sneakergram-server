b435b5ac6fd6215bf6ad20d42465abd7d919be7f
M Haidar Hanif <git@mhaidarhanif.com>
2020-07-31 13:40:59 +0700

    Accept file upload through /sneakers/upload

--- middlewares/sneakers/index.js
+++ middlewares/sneakers/index.js
@@ -4,8 +4,20 @@ const router = express.Router();
 const sneakers = require("./controllers");
 const auth = require("../auth/controllers");
 
+const { upload } = require("../../utils/multer");
+
 router.post("/seed", sneakers.seed);
 router.post("/", auth.isAuthenticated, sneakers.create);
 router.get("/", sneakers.getAll);
+
+router.post("/upload", upload.single("sneaker"), (req, res) => {
+  console.log(req.file, req.body);
+
+  res.status(201).send({
+    message: "Sneaker image uploaded",
+    file: req.file,
+    body: req.body,
+  });
+});
 
 module.exports = router;
